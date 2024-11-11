138cce96f6a8f695537077fde7ea7990c88efacf
josuem317 <jowebdesigc@gmail.com>
2024-11-11 11:26:02 -0400

    update contacts

--- src/isla/UpcomingDates.jsx
+++ src/isla/UpcomingDates.jsx
@@ -5,19 +5,19 @@ export function UpcomingDates() {
   <table className="rounded-3xl min-w-full">
     <thead className="font-bold">
       <tr>
-        <th className="py-3 px-4 border-b-2 border-gray-300 text-sm font-semibold text-center">Fecha</th>
-        <th className="py-3 px-4 border-b-2 border-gray-300 text-sm font-semibold text-center">Ciudad</th>
-        <th className="py-3 px-4 border-b-2 border-gray-300 text-sm font-semibold text-center">Lugar</th>
-        <th className="py-3 px-4 border-b-2 border-gray-300 text-sm font-semibold text-center">Tickets</th>
+        <th className="py-3 px-4 border-b-2 border-gray-300 md:text-sm text-[0.6rem] font-semibold text-center">Fecha</th>
+        <th className="py-3 px-4 border-b-2 border-gray-300 md:text-sm text-[0.6rem] font-semibold text-center">Ciudad</th>
+        <th className="py-3 px-4 border-b-2 border-gray-300 md:text-sm text-[0.6rem] font-semibold text-center">Lugar</th>
+        <th className="py-3 px-4 border-b-2 border-gray-300 md:text-sm text-[0.6rem] font-semibold text-center">Tickets</th>
       </tr>
     </thead>
     <tbody>
       <tr className="border-t font-bold">
-        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">8 de marzo</td>
-        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">Argentina</td>
-        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">C ART MEDIA</td>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.6rem]">8 de marzo</td>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.6rem]">Argentina</td>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.6rem]">C ART MEDIA</td>
         <td className="py-4 px-4 text-center bg-green-500 hover:bg-green-700 ">
-          <a className=" py-2 px-4 rounded-3xl h-full w-full text-white md:text-sm text-[0.7rem]" href="https://www.passline.com/eventos/magic-men-325883" target="_blank" rel="noopener noreferrer">Comprar entrada</a>
+          <a className=" py-2 px-4 rounded-3xl h-full w-full text-white md:text-sm text-[0.6rem] text-center" href="https://www.passline.com/eventos/magic-men-325883" target="_blank" rel="noopener noreferrer">Tickets</a>
         </td>
       </tr>
     </tbody>
