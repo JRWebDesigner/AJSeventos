92278194728fe36815f1e8f93f11b455311d8aeb
josuem317 <jowebdesigc@gmail.com>
2024-11-11 11:06:43 -0400

    updates touring dates

--- src/isla/UpcomingDates.jsx
+++ src/isla/UpcomingDates.jsx
@@ -1,8 +1,8 @@
 export function UpcomingDates() {
     return(
-        <div className="bg-gray-950 w-[90%] md:w-[40%] min-h-24 rounded-3xl m-auto mt-10 flex justify-center items-center">
+        <div className="bg-gray-950 w-[90%] md:w-[40%] min-h-24 rounded-3xl m-auto mt-10">
 <div className="overflow-x-auto">
-  <table className="rounded-3xl min-w-full bg-gray-800">
+  <table className="rounded-3xl min-w-full">
     <thead className="font-bold">
       <tr>
         <th className="py-3 px-4 border-b-2 border-gray-300 text-sm font-semibold text-center">Fecha</th>
@@ -13,11 +13,11 @@ export function UpcomingDates() {
     </thead>
     <tbody>
       <tr className="border-t font-bold">
-        <td className="py-4 px-4 border-r text-center text-sm">8 de marzo</td>
-        <td className="py-4 px-4 border-r text-center text-sm">Argentina</td>
-        <td className="py-4 px-4 border-r text-center text-sm">C ART MEDIA</td>
-        <td className="py-4 px-4 text-center bg-green-500 hover:bg-green-700 text-sm">
-          <a className=" py-2 px-4 rounded-3xl text-white" href="https://www.passline.com/eventos/magic-men-325883" target="_blank" rel="noopener noreferrer">Comprar entrada</a>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">8 de marzo</td>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">Argentina</td>
+        <td className="py-4 px-4 border-r text-center md:text-sm text-[0.7rem]">C ART MEDIA</td>
+        <td className="py-4 px-4 text-center bg-green-500 hover:bg-green-700 ">
+          <a className=" py-2 px-4 rounded-3xl h-full w-full text-white md:text-sm text-[0.7rem]" href="https://www.passline.com/eventos/magic-men-325883" target="_blank" rel="noopener noreferrer">Comprar entrada</a>
         </td>
       </tr>
     </tbody>
